3c82d5d8bf946b150d9aead0c1552f9c939a4e2e
rytc-curr01 <curriculum01@technicrayong.ac.th>
2024-06-27 15:18:28 +0700

    Update sim.js

--- sim.js
+++ sim.js
@@ -1,21 +1,138 @@
     function submit_click() {
-
-      //กรรมการคนที่ 1 ปวช.
+        
       if (document.getElementById('user').value != "" && 
           document.getElementById('pass').value != "") {
-
+            //กรรมการคนที่ 1 ปวช.
             if (document.getElementById('user').value == "pvc01" && 
-          document.getElementById('pass').value == "pvc01*67*99") {
-            //document.getElementById('output').style.display = "block"
-            //document.getElementById('output2').innerHTML = 'https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing'  
-                //location.replace("https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing")
+                document.getElementById('pass').value == "pvc01*67*99") {
                 location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
-                //location.assign('https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing')
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 2 ปวช.
+            if (document.getElementById('user').value == "pvc02" && 
+                document.getElementById('pass').value == "pvc02*67*88") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 3 ปวช.
+            if (document.getElementById('user').value == "pvc03" && 
+                document.getElementById('pass').value == "pvc03*67*77") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 4 ปวช.
+            if (document.getElementById('user').value == "pvc04" && 
+                document.getElementById('pass').value == "pvc04*67*66") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 5 ปวช.
+            if (document.getElementById('user').value == "pvc05" && 
+                document.getElementById('pass').value == "pvc05*67*55") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 6 ปวช.
+            if (document.getElementById('user').value == "pvc06" && 
+                document.getElementById('pass').value == "pvc06*67*44") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 7 ปวช.
+            if (document.getElementById('user').value == "pvc07" && 
+                document.getElementById('pass').value == "pvc07*67*33") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 8 ปวช.
+            if (document.getElementById('user').value == "pvc08" && 
+                document.getElementById('pass').value == "pvc08*67*22") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 1 ปวส.
+            if (document.getElementById('user').value == "pvs01" && 
+                document.getElementById('pass').value == "pvs01*67*99") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 2 ปวส.
+            if (document.getElementById('user').value == "pvs02" && 
+                document.getElementById('pass').value == "pvs02*67*88") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }        
+            //กรรมการคนที่ 3 ปวส.
+            if (document.getElementById('user').value == "pvs03" && 
+                document.getElementById('pass').value == "pvs03*67*77") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+              //กรรมการคนที่ 4 ปวส.
+            if (document.getElementById('user').value == "pvs04" && 
+                document.getElementById('pass').value == "pvs04*67*66") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }        
+            //กรรมการคนที่ 5 ปวส.
+            if (document.getElementById('user').value == "pvs05" && 
+                document.getElementById('pass').value == "pvs05*67*55") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 6 ปวส.
+            if (document.getElementById('user').value == "pvs06" && 
+                document.getElementById('pass').value == "pvs06*67*44") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 7 ปวส.
+            if (document.getElementById('user').value == "pvs07" && 
+                document.getElementById('pass').value == "pvs07*67*33") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
+            }
+            else {
+              alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
+            }
+            //กรรมการคนที่ 8 ปวส.
+            if (document.getElementById('user').value == "pvs08" && 
+                document.getElementById('pass').value == "pvs08*67*22") {
+                location.href = "https://docs.google.com/spreadsheets/d/1aMyGoXIeeXY9ZoKOetLx0-RbwRSPUsJSOGKgWP6M4xQ/edit?usp=sharing"
             }
             else {
               alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
             }
 
+          
+          
       } else {
         alert('กรุณาป้อนข้อมูลให้ถูกต้อง')
       }
